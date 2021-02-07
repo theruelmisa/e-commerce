e26b33529d1b5a75b324430265a010f9979be6b8
Rumies <rumies.media@gmail.com>
2021-02-06 23:49:48 -0500

    integrated commerce js into project. Created multiple items through commerce js. Made call to api for list of items and shopping cart. Created Cart and CartItem component to display contents of shopping cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import { Navbar, Products } from './components';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
+import { Navbar, Products, Cart } from './components';
 
 const App = () => {
+    const [ products, setProducts ] = useState([]);
+    const [ cart, setCart ] = useState({});
+
+    useEffect(() => {
+        fetchProducts();
+        fetchCart();
+    }, []);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+        
+        setProducts(data);
+    }
+
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve());
+    }
+
+    const addToCartHandler = async ( productId, quantity ) => {
+        const item = await commerce.cart.add( productId, quantity );
+        setCart(item.cart);
+    }
+
+    console.log(cart)
+
     return ( 
         <div>
-            <Navbar />
-            <Products />
+            <Navbar totalItems={ cart.total_items } />
+            {/* <Products products={ products } onAddToCart={addToCartHandler} /> */}
+            <Cart cart={ cart } />
         </div>
     );
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,19 +3,14 @@ import { Grid } from '@material-ui/core'
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'swimsuit 1', description: 'one piece swimsuit.', price: '$65', image: 'https://images.unsplash.com/photo-1520065949650-380765513210?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80' },
-    { id: 2, name: 'swimsuit 2', description: 'two piece swimsuit.', price: '$85', image: 'https://images.unsplash.com/photo-1522322904670-5cf2a8529d21?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80' }
-];
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
 
     const classes = useStyles();
 
     const renderedProducts = products.map( product => (
 
         <Grid key={ product.id } item xs={ 12 } sm= { 6 } md={ 4 } lg={ 3 }>
-            <Product product={ product } />
+            <Product product={ product } onAddToCart={onAddToCart} />
         </Grid>   
     ));
 
